cac169f85cbb9ce5a7281ddb664a563a1199bdaf
crocme10 <matt@area403.org>
2023-06-19 18:00:21 +0200

    spec.yaml, dockerfile

---
+++ zero2prod/docker/zero2prod/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -Eeo pipefail
+
+_main() {
+	if [ "$1" = 'run' ]; then
+    /srv/zero2prod/bin/zero2prod --config-dir /srv/zero2prod/etc/zero2prod run
+  fi
+}
+
+_main "$@"
+
