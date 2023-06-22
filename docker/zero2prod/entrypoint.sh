7fb211e29012d44057f445dbc3ff98b6ba87d1e6
crocme10 <matt@area403.org>
2023-06-22 13:59:44 +0200

    testing environment

--- docker/zero2prod/entrypoint.sh
+++ docker/zero2prod/entrypoint.sh
@@ -2,6 +2,17 @@
 
 set -Eeo pipefail
 
+echo "${newsletter.HOSTNAME}"
+echo "${newsletter.PORT}"
+echo "${newsletter.USERNAME}"
+echo "${newsletter.PASSWORD}"
+echo "------"
+echo "${ZERO2PROD__DATABASE_HOST}"
+echo "${ZERO2PROD__DATABASE_PORT}"
+echo "${ZERO2PROD__DATABASE_USERNAME}"
+echo "${ZERO2PROD__DATABASE_PASSWORD}"
+echo "------"
+
 _main() {
 	if [ "$1" = 'run' ]; then
     /srv/zero2prod/bin/zero2prod --config-dir /srv/zero2prod/etc/zero2prod config
