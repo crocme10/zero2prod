e803299829cca3f78c4ef7e3f2f8d0a12c2fcdf0
crocme10 <matt@area403.org>
2023-06-22 13:52:14 +0200

    testing environment

--- docker/zero2prod/entrypoint.sh
+++ docker/zero2prod/entrypoint.sh
@@ -4,7 +4,7 @@ set -Eeo pipefail
 
 _main() {
 	if [ "$1" = 'run' ]; then
-    /srv/zero2prod/bin/zero2prod --config-dir /srv/zero2prod/etc/zero2prod run
+    /srv/zero2prod/bin/zero2prod --config-dir /srv/zero2prod/etc/zero2prod config
   fi
 }
 
