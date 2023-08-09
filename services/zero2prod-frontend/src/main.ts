a2bf3d38871159ec1ddec4dca7419d9e39dca13c
crocme10 <matt@area403.org>
2023-08-09 09:27:27 +0200

    remove global route

--- services/zero2prod-frontend/src/main.ts
+++ services/zero2prod-frontend/src/main.ts
@@ -7,9 +7,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-// import RouterInterceptor from './plugins/Router'
 
-// const app = createApp(App).use(createPinia()).use(router).use(RouterInterceptor)
 const app = createApp(App).use(createPinia()).use(router)
 
 app.mount('#app')
