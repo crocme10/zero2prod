f0a2a36cd72b8da0a6ef3ad32fba9df21e83ebc9
crocme10 <matt@area403.org>
2023-08-29 14:26:18 +0200

    cleanup

--- services/zero2prod-frontend/src/components/Home.vue
+++ services/zero2prod-frontend/src/components/Home.vue
@@ -9,10 +9,10 @@
       <p class="xl:text-xl tracking-wider text-slate-700 font-text font-bold">Software Consulting Company</p>
       <!--<a href="#" class="font-text text-white sm:font-xl uppercase py-3 px-6 sm:py-4 sm:px-8 rounded-full shadow-lg bg-slate-900 hover:bg-slate-800 mt-8 sm:mt-16">Coming Soon</a>-->
     </div>
-    <div class="w-1/2 flex flex-col items-start pl-24 py-24 sm:py-0">
-      <RouterLink to="/about" class="hidden xl:block font-text font-bold uppercase text-gray-800 tracking-wider px-4 xl:px-8 py-6 text-lg hover:underline">About</RouterLink>
-      <RouterLink to="/about" class="hidden xl:block font-text font-bold uppercase text-gray-800 tracking-wider px-4 xl:px-8 py-6 text-lg hover:underline">Projects</RouterLink>
-      <RouterLink to="/contact" class="hidden xl:block font-text font-bold uppercase text-gray-800 tracking-wider px-4 xl:px-8 py-6 text-lg hover:underline">Contact</RouterLink>
+    <div class="w-1/2 flex flex-col items-start sm:pl-24 py-24 sm:py-0">
+      <RouterLink to="/about" class="xl:block font-text font-bold uppercase text-gray-800 tracking-wider px-4 xl:px-8 py-6 text-lg hover:underline">About</RouterLink>
+      <RouterLink to="/about" class="xl:block font-text font-bold uppercase text-gray-800 tracking-wider px-4 xl:px-8 py-6 text-lg hover:underline">Projects</RouterLink>
+      <RouterLink to="/contact" class="xl:block font-text font-bold uppercase text-gray-800 tracking-wider px-4 xl:px-8 py-6 text-lg hover:underline">Contact</RouterLink>
     </div>
   </div>
 </template>
